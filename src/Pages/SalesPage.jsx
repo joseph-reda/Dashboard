1f9649bb7628f1af561e1583fbb78486743fd951
Joseph Reda Rizk <143938394+joseph-reda@users.noreply.github.com>
2025-02-12 03:02:13 +0200

    update pageName

--- src/Pages/SalesPage.jsx
+++ src/Pages/SalesPage.jsx
@@ -18,7 +18,7 @@ const salesStats = {
 const SalesPage = () => {
   return (
     <div className="flex-1 overflow-auto relative z-10">
-      <Header title="Sales Dashboard" />
+      <Header pageName="Sales Dashboard" />
 
       <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
         {/* SALES STATS */}
